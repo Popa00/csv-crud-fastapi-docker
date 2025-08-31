0c3fc8cfde7b6406cbcb41871a417ecd5c756be1
Popa00 <popandonovj@gmail.com>
2025-08-31 14:05:38 +0200

    fine

--- app/main.py
+++ app/main.py
@@ -180,4 +180,4 @@ async def elimina(id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
